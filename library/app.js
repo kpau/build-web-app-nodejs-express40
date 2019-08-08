abb07c7064777059435e1d3c77a2113852c20f9b
kpau <krs.mnt@gmail.com>
2019-08-08 16:48:09 +0300

    some formatting...

--- library/app.js
+++ library/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
-const morgan = require('morgan');
+const morganDebug = require('morgan-debug');
 const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morganDebug('http', 'tiny'));
 app.use(express.static(path.join(__dirname, 'public/')));
 
 // load from node_modules
